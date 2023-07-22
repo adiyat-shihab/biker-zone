c20dd7266cc101dd0fbb853ba3e8c691ce9aebc4
adiyat-shihab <mohammadshihab104@gmail.com>
2023-07-22 22:27:15 +0600

    first commit

---
+++ practice.js
@@ -0,0 +1,4 @@
+// first time in javascript, for learning purposes only
+let shihab = "adiyat";
+const adiyat = "takwa";
+console.log((shihab += 2));
---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        "regal-liner": "rgba(24, 24, 24, 0.82)",
+        "regal-liner-light": "rgba(24, 24, 24, 0.00)",
+      },
+    },
+  },
+};
+module.exports = {
+  theme: {
+    fontFamily: {
+      sans: [
+        "Inter var, sans-serif",
+        {
+          popins: "font-family: 'Poppins', sans-serif;",
+        },
+      ],
+    },
+  },
+};
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        popins: ["font-family: Poppins, sans-serif;"],
+      },
+    },
+  },
+};
